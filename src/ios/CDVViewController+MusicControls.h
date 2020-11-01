2937266670c24e560d29a1cd5555a742cc146c81
Jan Peeters <jan@interbrug.nl>
2020-11-01 15:02:33 +0100

    Update and rename MainViewController+MusicControls.h to CDVViewController+MusicControls.h

---
+++ src/ios/CDVViewController+MusicControls.h
@@ -0,0 +1,19 @@
+//
+//  MainViewController+MusicControls.h
+//  
+//
+//  Created by Juan Gonzalez on 12/17/16.
+//
+//
+
+#ifndef CDVViewController_MusicControls_h
+#define CDVViewController_MusicControls_h
+
+#import "CDVViewController.h"
+
+@interface CDVViewController (MusicControls)
+
+@end
+
+
+#endif /* CDVViewController_MusicControls_h */
